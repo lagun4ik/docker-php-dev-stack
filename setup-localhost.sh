5c3f8fb8c1dec3ae6241265b55978a5dd517873e
lagun4ik <ivan@lagunovsky.com>
2016-01-15 18:53:00 +0300

    fix bugs DevManager

--- setup-localhost.sh
+++ setup-localhost.sh
@@ -5,10 +5,10 @@ apt-get install -y apache2-utils unzip wget
 htpasswd -c ./data/nginx/.htpasswd root
 
 git clone https://github.com/codeb2cc/phpMemcachedAdmin ./data/www/localhost/phpMemcachedAdmin
-sed -i "s/127\.0\.0\.1/memcached)/g" ./data/www/localhost/phpMemcachedAdmin/configure.php
+sed -i "s/127\.0\.0\.1/memcached)/g" ./data/www/localhost/phpMemcachedAdmin/Config/Memcache.php
 
 sed -i "s/#//g" ./data/nginx/localhost.conf
-sed -i "s/deny/#deny)/g" ./data/nginx/localhost.conf
+sed -i "s/deny/#deny/g" ./data/nginx/localhost.conf
 
 wget https://github.com/phpmyadmin/phpmyadmin/archive/RELEASE_4_4_15_2.zip
 unzip RELEASE_4_4_15_2.zip -d ./data/www/localhost
@@ -19,4 +19,4 @@ mv ./data/www/localhost/phpmyadmin/config.sample.inc.php ./data/www/localhost/ph
 sed -i -r "s/$cfg\['blowfish_secret'\] = ''/$cfg\['blowfish_secret'\] = '$(openssl rand -base64 32)'/g" ./data/www/localhost/phpmyadmin/config.inc.php
 sed -i -r "s/localhost/mysql/g" ./data/www/localhost/phpmyadmin/config.inc.php
 
-docker-compose restart
+echo "restart nginx"
