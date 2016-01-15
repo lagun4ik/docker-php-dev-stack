7dafbcfef2190221aa1516b15688d47dcf1ff372
lagun4ik <ivan@lagunovsky.com>
2016-01-15 18:21:27 +0300

    fix

--- setup-localhost.sh
+++ setup-localhost.sh
@@ -1,19 +1,22 @@
 #/bin/bash
 
 apt-get update
-apt-get install -y apache2-utils
-htpasswd -c /data/nginx/.htpasswd exampleuser
+apt-get install -y apache2-utils unzip wget
+htpasswd -c ./data/nginx/.htpasswd root
 
 git clone https://github.com/codeb2cc/phpMemcachedAdmin ./data/www/localhost/phpMemcachedAdmin
-sed -i "s/127\.0\.0\.1/memcached)/g" ./data/www/localhost/phpMemcachedAdmin
+sed -i "s/127\.0\.0\.1/memcached)/g" ./data/www/localhost/phpMemcachedAdmin/configure.php
 
 sed -i "s/#//g" ./data/nginx/localhost.conf
 sed -i "s/deny/#deny)/g" ./data/nginx/localhost.conf
 
-git clone https://github.com/phpmyadmin/phpmyadmin.git ./data/www/localhost/phpmyadmin
+wget https://github.com/phpmyadmin/phpmyadmin/archive/RELEASE_4_4_15_2.zip
+unzip RELEASE_4_4_15_2.zip -d ./data/www/localhost
+mv ./data/www/localhost/phpmyadmin-RELEASE_4_4_15_2 ./data/www/localhost/phpmyadmin
+rm RELEASE_4_4_15_2.zip
 mv ./data/www/localhost/phpmyadmin/config.sample.inc.php ./data/www/localhost/phpmyadmin/config.inc.php
 
 sed -i -r "s/$cfg\['blowfish_secret'\] = ''/$cfg\['blowfish_secret'\] = '$(openssl rand -base64 32)'/g" ./data/www/localhost/phpmyadmin/config.inc.php
 sed -i -r "s/localhost/mysql/g" ./data/www/localhost/phpmyadmin/config.inc.php
 
- docker-compose restart
+docker-compose restart
